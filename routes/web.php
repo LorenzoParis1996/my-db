acb4256805aa17fb850a3d4749b7b4b5b16a57b1
LorezoParis1996 <lorenzoparis86@live.it>
2024-09-30 11:10:58 +0200

    created seeder, model and table for "anime"

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->name('admin.')->prefix('admin/')->group(
+    function() {
+        Route::get('home', [AdminHomeController::class, 'index'])->name('home')->middleware('auth');
+    }
+);
