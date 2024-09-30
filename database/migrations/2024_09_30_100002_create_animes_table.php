cd2199894258228e0a9234e039b012ee8fe58948
LorezoParis1996 <lorenzoparis86@live.it>
2024-09-30 12:02:02 +0200

    fixed name of migration and seeded correctly

--- database/migrations/2024_09_30_100002_create_animes_table.php
+++ database/migrations/2024_09_30_100002_create_animes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('anime', function (Blueprint $table) {
+        Schema::create('animes', function (Blueprint $table) {
             $table->id();
             $table->string('title', 255);
             $table->string('original_title', 255);
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('anime');
+        Schema::dropIfExists('animes');
     }
 };
--- database/seeders/AnimeSeeder.php
+++ database/seeders/AnimeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Anime;
 
 class AnimeSeeder extends Seeder
 {
@@ -12,6 +13,52 @@ class AnimeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $animes = [
+            [
+                'title'=> "Jojo's bizarre adventure stone ocean",
+                'original_title'=> "ジョジョの奇妙な冒険 ストーンオーシャン, Jojo no kimyō na bōken - Sutōn ōshan",
+                'release_year'=> "2021"
+            ],
+            [
+                'title'=> "Nana",
+                'original_title'=> "NANA ナナ",
+                'release_year'=> "2006"
+            ],
+            [
+                'title'=> "Cowboy Bebop",
+                'original_title'=> "カウボーイビバップ?, Kaubōi Bibappu",
+                'release_year'=> "1998"
+            ],
+            [
+                'title'=> "Neon Genesis Evangelion",
+                'original_title'=> "新世紀エヴァンゲリオン, Shin seiki Evangerion",
+                'release_year'=> "1995"
+            ],
+            [
+                'title'=> "Attack on Titan",
+                'original_title'=> "進撃の巨人, Shingeki no kyojin",
+                'release_year'=> "2013"
+            ],
+            [
+                'title'=> "Death Note",
+                'original_title'=> "デスノート, Desu Nōto",
+                'release_year'=> "2006"
+            ],
+            [
+                'title'=> "Hajime no Ippo",
+                'original_title'=> "はじめの一歩",
+                'release_year'=> "2000"
+            ],
+            [
+                'title'=> "Full Metal Alchemist: Brotherhood",
+                'original_title'=> "鋼の錬金術師 FULLMETAL ALCHEMIST, Hagane no renkinjutsushi - FULLMETAL ALCHEMIST",
+                'release_year'=> "2009"
+            ],
+
+        ];
+
+        foreach($animes as $anime){
+            Anime::create($anime);
+        }
     }
 }
