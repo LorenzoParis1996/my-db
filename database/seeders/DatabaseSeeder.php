5cc48fc5de5787790be7514c97194749f37975a9
LorezoParis1996 <lorenzoparis86@live.it>
2024-11-28 13:10:29 +0100

    seeder populated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             AudienceSeeder::class,
             StudioSeeder::class,
             AnimeSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +13,67 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $products = [
+           [
+            'name'=> "Jojo's Bizarre Adventure Stone Ocean",
+            'season'=> "Season 6",
+            'format'=> "Blu-Ray",
+            'price'=> 17.99,
+            'stock'=> 5,
+           ],
+           [
+            'name'=> "Nana",
+            'season'=> "Complete season",
+            'format'=> "DVD",
+            'price'=> 12.99,
+            'stock'=> 3,
+           ],
+           [
+            'name'=> "Cowboy Bebop",
+            'season'=> "Complete season",
+            'format'=> "DVD",
+            'price'=> 12.99,
+            'stock'=> 6,
+           ],
+           [
+            'name'=> "Neon Genesis Evangelion",
+            'season'=> "Complete season",
+            'format'=> "DVD",
+            'price'=> 12.99,
+            'stock'=> 2,
+           ],
+           [
+            'name'=> "Attack on Titan",
+            'season'=> "Season 1",
+            'format'=> "Blu-Ray",
+            'price'=> 17.99,
+            'stock'=> 4,
+           ],
+           [
+            'name'=> "Death Note",
+            'season'=> "Complete season",
+            'format'=> "DVD",
+            'price'=> 12.99,
+            'stock'=> 5,
+           ],
+           [
+            'name'=> "Hajime no Ippo",
+            'season'=> "Complete season",
+            'format'=> "DVD",
+            'price'=> 12.99,
+            'stock'=> 3,
+           ],
+           [
+            'name'=> "Fullmetal Alchemist: Brotherhood",
+            'season'=> "Complete season",
+            'format'=> "Blu-Ray",
+            'price'=> 17.99,
+            'stock'=> 4,
+           ],
+        ];
+
+        foreach($products as $product) {
+            Product::create($product);
+        }
     }
 }
